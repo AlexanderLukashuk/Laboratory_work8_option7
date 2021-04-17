0df0041d5e8e6cf592930921db4b5fc0dd30d113
Саня <sanya@Sergejs-MacBook-Air.local>
2021-04-17 22:07:31 +0600

    Созданы переменная для ввода, размер массива и сам массив, создан цикл for для запомления значениями элементы массива

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace Task8var7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //* 1. Ввести n чисел. Найти количество чисел на отрезке [10;20].
+            
+            string input;
+            int arraySize;
+            Console.Write("Введите размер массива: ");
+            input = Console.ReadLine();
+            arraySize = int.Parse(input);
+            int[] numbers = new int[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                Console.Write("Введите число: ");
+                input = Console.ReadLine();
+                numbers[i] = int.Parse(input);
+            }
         }
     }
 }
